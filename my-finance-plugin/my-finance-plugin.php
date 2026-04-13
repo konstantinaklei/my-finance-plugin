cafabdf10a62ce190ec65fd073ae372e15d18866
Konstantinaklei <juniorklei2@gmail.com>
2026-04-13 14:22:23 +0300

    Add GPLv2 license to plugin header to pass PCP checks

--- my-finance-plugin/my-finance-plugin.php
+++ my-finance-plugin/my-finance-plugin.php
@@ -4,6 +4,8 @@
  * Description: Σύστημα Διαχείρισης Εσόδων - Εξόδων.
  * Version: 1.0
  * Author: Konstantina
+ * License: GPL2
+ * License URI: https://www.gnu.org/licenses/gpl-2.0.html
  */
 
 if (!defined('ABSPATH')) exit;
